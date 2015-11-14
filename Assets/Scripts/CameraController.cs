2cc1dcd654e7b7b1e29db7c0f29b849347380451
whiteSkar <sypower07@hotmail.com>
2015-11-14 13:57:12 -0800

    add everything

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+
+	private Vector3 offset;
+	
+	void Start () 
+	{
+		this.offset = this.transform.position - player.transform.position;
+	}
+
+	void LateUpdate () 
+	{
+		this.transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	void Update() 
+	{
+		this.transform.Rotate(new Vector3(45, 45, 0) * Time.deltaTime);
+	}
+}
