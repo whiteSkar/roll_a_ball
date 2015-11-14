6afbbb7919f86f1b5f3c65ecb1a8d925c98404f4
whiteSkar <sypower07@hotmail.com>
2015-11-14 14:17:01 -0800

    Format scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,50 +2,48 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
-	public float speed;
-	public Text countText;
-	public Text winText;
-	public int totalPickUpCount;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public int totalPickUpCount;
 
-	private Rigidbody rb;
-	private int count;
+    private Rigidbody rb;
+    private int count;
 
-	void Start()
-	{
-		rb = GetComponent<Rigidbody>();
-		count = 0;
-		this.UpdateCountText();
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        this.UpdateCountText();
 
-		this.winText.text = "";
-	}
+        this.winText.text = "";
+    }
 
-	void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce(movement * speed);
-	}
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
 
-	void OnTriggerEnter(Collider other) 
-	{
-		if (other.gameObject.CompareTag("Pick Up"))
-		{
-			other.gameObject.SetActive(false);
-			count++;
-			this.UpdateCountText();
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            count++;
+            this.UpdateCountText();
+        }
+    }
 
-	void UpdateCountText()
-	{
-		countText.text = "Count: " + count.ToString();
-		if (count >= totalPickUpCount)
-		{
-			this.winText.text = "You Win!";
-		}
-	}
+    void UpdateCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= totalPickUpCount) {
+            this.winText.text = "You Win!";
+        }
+    }
 }
